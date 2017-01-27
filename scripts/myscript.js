b76edb34dd03725bbc92443b31649eedcb7bb852
praneetsharma <praneet.223@gmail.com>
2017-01-27 13:59:46 +0530

    cleaned up existing code and added a few hashmaps for future development

--- scripts/myscript.js
+++ scripts/myscript.js
@@ -2,15 +2,51 @@ $(document).ready(function() {
 
   var BASE_URL = "http://api.openweathermap.org/data/2.5/weather";
   var APP_KEY = "b551c8f5d7dbf3bae27399d06c364303";
+  var SPACE = " ";
 
-  var TEMPERATURE_UNIT = {
+  // Mapping of open-weather's names to my own weather names
+  // The rest of the maps will be using my weather names to get me things.
+  // This way I can separate out rest of my logic from how a particular
+  // website calls the weather names.
+  var openWeather_myWeather_map = {
+    "Thunderstorm": "thunderstorm",
+    "Drizzle": "drizzle",
+    "Rain": "rain",
+    "Snow": "snow",
+    "Atmosphere": "haze",
+    "Clear": "sunny",
+    "Clouds": "clouds"
+  };
+
+  // hash map of what background color to choose
+  // given the weather type
+  var weather_backColor_map = {
+    "thunderstorm": "#F4F6F6",
+    "drizzle": "#AED6F1",
+    "rain": "#5DADE2",
+    "snow": "#F4F6F7",
+    "haze": "#95A5A6",
+    "sunny": "#F4D03F",
+    "clouds": "#E5E7E9"
+  };
+
+  var TEMPERATURE_UNIT_MAP = {
     "celcius": "C",
     "farenheit": "F"
   };
-  var DEFAULT_TEMPERATURE_UNIT = TEMPERATURE_UNIT.celcius;
+  var TEMPERATURE_UNIT = TEMPERATURE_UNIT_MAP.celcius;
 
-  var LATITUDE = 0.0;
-  var LONGITUDE = 0.0;
+  function getBackgroundColor(type) {
+    return weather_backColor_map[openWeather_myWeather_map[type]];
+  }
+
+  function toggleTemperatureUnit () {
+    if (TEMPERATURE_UNIT === TEMPERATURE_UNIT_MAP.celcius) {
+      TEMPERATURE_UNIT = TEMPERATURE_UNIT_MAP.farenheit;
+    } else {
+      TEMPERATURE_UNIT = TEMPERATURE_UNIT_MAP.celcius;
+    }
+  }
 
   // converts kelvin to celcius and rounds off to 2 decimal places
   function kelvinToCelcius (kel) {
@@ -31,13 +67,13 @@ $(document).ready(function() {
     navigator.geolocation.getCurrentPosition(successGetLocation, errorGetLocation);
   }
   function successGetLocation (position) {
-    LATITUDE = position.coords.latitude;
-    LATITUDE = LATITUDE.toFixed(1); // rounding off to single decimal
+    var latitude = position.coords.latitude;
+    var longitude = position.coords.longitude;
 
-    LONGITUDE = position.coords.longitude;
-    LONGITUDE = LONGITUDE.toFixed(1); // rounding off to single decimal
+    latitude = latitude.toFixed(1); // rounding off to single decimal
+    longitude = longitude.toFixed(1); // rounding off to single decimal
 
-    var url = BASE_URL + "?lat=" + LATITUDE + "&lon=" + LONGITUDE + "&APPID=" + APP_KEY;
+    var url = BASE_URL + "?lat=" + latitude + "&lon=" + longitude + "&APPID=" + APP_KEY;
 
     getWeatherInfo(url);
   }
@@ -49,8 +85,12 @@ $(document).ready(function() {
     $.ajax({
       url: url_,
       success: function (result) {
+
+        $("body").css("background-color", getBackgroundColor(result.weather[0].main));
+
         $(".weather-type").html(result.weather[0].main);
-        $(".weather-temp").html(kelvinToCelcius(result.main.temp) + DEFAULT_TEMPERATURE_UNIT);
+        $(".temperature-value").html(kelvinToCelcius(result.main.temp));
+        $(".temperature-unit").html(TEMPERATURE_UNIT);
       },
       error: function (err) {
         alert(err.code + ":" + err.message);
